9c135a8379f4c2a0b490328b901077d0693893de
chauhan-123 <sumitchauan111@gmail.com>
2019-12-18 01:05:04 +0530

    pincode api functionlity

--- src/app/modules/layout/subscribe/subscribe.service.ts
+++ src/app/modules/layout/subscribe/subscribe.service.ts
@@ -1,32 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from '../../shared/services/http.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { PaymentModel } from '../../shared/models/payment.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubscribeService {
-  baseUrl = "http://localhost:3000/"
-  constructor( private _http:HttpService, private httpClient:HttpClient) { }
+  baseUrl = "http://localhost:3000/";
+  pincodeUrl = "https://api.postalpincode.in/pincode/";
+  constructor(private _http: HttpService, private httpClient: HttpClient) { }
 
 
   // cash on delivery method
-  cashOnDelivery(data){
-return this._http.post(`${this.baseUrl}cashOnDelivery` , data);
+  cashOnDelivery(data) {
+    return this._http.post(`${this.baseUrl}cashOnDelivery`, data);
   }
 
   //  search button method for get the data all pincode     
-  searchButtonData(data){
-    console.log(data.pincode)
-    return this.httpClient.get(this.baseUrl + `getPincodeData`, { params: data });
+  searchButtonData(data) {
+    return this.httpClient.get(this.pincodeUrl + data.pincode);
   }
 
-
-//  debit card payment online
-
+  //  debit card payment online
   createPayment(paymentRequest: PaymentModel) {
-    console.log(paymentRequest , '=============')
+    console.log(paymentRequest, '=============')
     // const PAYMENT_URL = '/payment/pay';
     return this._http.post(this.baseUrl, paymentRequest);
   }
